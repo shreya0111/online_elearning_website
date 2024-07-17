8afe6ae52402a9cf811044494b3e24a5004f60eb
Shreya Dey <98154027+shreya0111@users.noreply.github.com>
2024-07-17 19:36:20 +0530

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+    "use strict";
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+   
+
+    
+})(jQuery);
